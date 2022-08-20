feb2d0e88bcaf553284c80f835c5107bbfa33e6b
bob <simoelk@t-online.de>
2022-08-20 22:23:50 +0200

    rps-game.js

--- rps-game.js
+++ rps-game.js
@@ -140,8 +140,8 @@ function checkWin() {
 
 
 function updateMoves(playerSelection, computerSelection) {
-    document.getElementById("img-play").src = `./images/${playerSelection}.jpg`;
-    document.getElementById("img-comp").src = `./images/${computerSelection}.jpg`;  
+    document.getElementById("img-play").src = `images/${playerSelection}.jpg`;
+    document.getElementById("img-comp").src = `images/${computerSelection}.jpg`;  
 }
 
 
